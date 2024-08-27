573054cdc90738abac47ca5bea3ee2158d437586
Sebastian Galarza <95152236+sebferreira@users.noreply.github.com>
2024-08-26 21:22:36 -0300

    Update ayuda.jsx

--- front/src/pages/Ayuda/ayuda.jsx
+++ front/src/pages/Ayuda/ayuda.jsx
@@ -48,7 +48,7 @@ export default function Ayuda() {
                 fontWeight: "bold",
                 color: "black",
               }}>
-              Ayuda
+              Preguntas frecuentes
             </Typography>
             {!user && (
               <>
