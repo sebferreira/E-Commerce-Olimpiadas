3558686700494e8b19227c41dee88e017ab3570a
Sebastian Galarza <95152236+sebferreira@users.noreply.github.com>
2024-08-27 22:38:23 -0300

    Update sportSection.jsx

--- front/src/components/ProductSection/sportSection.jsx
+++ front/src/components/ProductSection/sportSection.jsx
@@ -51,7 +51,7 @@ export default function SportsSection() {
                 xl={2.5} */
                 key={producto.id_producto}
                 sx={{
-                  backgroundColor: "#F5F5F5",
+                  backgroundColor: "#FFFF",
                   borderRadius: 2,
                   boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
                   cursor: "pointer",
@@ -66,7 +66,7 @@ export default function SportsSection() {
                   justifyContent: {xs: "space-evenly", sm: "space-between"},
                   color: "black",
                   padding: ".5rem",
-                  height: {xs: "250px", sm: "320px"},
+                  height: {xs: "300px", sm: "320px"},
                   width: {xs: "200px", sm: "250px"},
                 }}>
                 <Card producto={producto} />
