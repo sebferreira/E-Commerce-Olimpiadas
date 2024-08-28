cdcea0782e7a33de8c37f2859e8126d6dc7b0f2b
Sebastian Galarza <95152236+sebferreira@users.noreply.github.com>
2024-08-27 22:36:54 -0300

    Update carritoSectionAdmin.jsx

--- front/src/components/ProductSection/carritoSectionAdmin.jsx
+++ front/src/components/ProductSection/carritoSectionAdmin.jsx
@@ -97,7 +97,7 @@ export default function CarritoSectionAdmin() {
                   <Box
                     key={pedido.id_pedido}
                     sx={{
-                      backgroundColor: "#F5F5F5",
+                      backgroundColor: "#FFFF",
                       borderRadius: 2,
                       boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
                       cursor: "pointer",
@@ -112,7 +112,7 @@ export default function CarritoSectionAdmin() {
                       justifyContent: {xs: "space-evenly", sm: "space-between"},
                       color: "black",
                       padding: ".5rem",
-                      height: {xs: "250px", sm: "320px"},
+                      height: {xs: "300px", sm: "320px"},
                       width: {xs: "200px", sm: "250px"},
                     }}>
                     <CardCarrito
